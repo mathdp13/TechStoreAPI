25d376adee788b8797e2e0d370cf859ea5810ea9
Matheus de Paula Silva <sillva130@gmail.com>
2026-02-25 00:04:29 -0300

    feat: dockerização completa da API e ajuste de compatibilidade .NET 8

--- TechStore.Api/Program.cs
+++ TechStore.Api/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using TechStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                       ?? "Data Source=techstore.db";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=techstore.db"));
@@ -10,23 +12,31 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options => {
-    options.AddPolicy("PermitirTudo",
-        policy => policy.AllowAnyOrigin()
-        .AllowAnyHeader()
-        .AllowAnyMethod());
+    options.AddPolicy("Producao", policy => 
+    {
+        policy.WithOrigins(
+                "http://localhost:5500", 
+                "http://127.0.0.1:5500",
+                "https://meu-site.com"      // Caso futuramente colocar uma URL
+              )
+              .WithMethods("GET", "POST", "PUT", "DELETE")
+              .WithHeaders("Content-Type", "Authorization");
+    });
 });
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c => 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TechStore API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
-app.UseCors("PermitirTudo");
+app.UseCors("Producao");
 app.MapControllers();
 
 app.Run();
